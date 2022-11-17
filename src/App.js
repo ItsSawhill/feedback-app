bc34e18b92ecf687bbf57bda0e92e6e3e2185e71
sahil parab <sahiparab6117@gmail.com>
2022-11-17 09:03:44 +0530

    modified:   public/index.html 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+    const title ="Blog post";
+    const body= "This is my blog post";
+    const comments =[
+        {id:1,text:"comment one"},
+        {id:2,text:"comment two"},
+        {id:3,text:"comment three"}
+
+    ]
+
+    const loading= false;
+    if(loading) return <h1>Loading...</h1>
+    const showcomment = true;
+    const commentBlock=(<div>
+        <h3>Comments ({comments.length})</h3> 
+         <ul>
+         {comments.map((comment,index)=>(
+             <li key={index}>{comment.text}</li>
+         ))}
+         </ul>
+     </div>)
+    return (<div className='container'>
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+       {showcomment ? commentBlock:'NO'}
+       
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
