281cc2587f18e4db97aec348c9d7f77a6949f75c
sahil parab <sahiparab6117@gmail.com>
2023-04-01 10:38:56 +0530

    most of the project done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
+import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import {useState} from "react"
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 import React from 'react'
+import AboutPages from './pages/AboutPages'
+import AboutIconLink from './components/AboutIconLink'
 
 function App(){
-    const title ="Blog post";
-    const body= "This is my blog post";
-    const comments =[
-        {id:1,text:"comment one"},
-        {id:2,text:"comment two"},
-        {id:3,text:"comment three"}
+    const[feedback,setFeedback]= useState(FeedbackData)
+    
+    const addFeedback=(newFeedback)=>{
+           newFeedback.id = uuidv4()
+           setFeedback([newFeedback,...feedback])
+        }
 
-    ]
-
-    const loading= false;
-    if(loading) return <h1>Loading...</h1>
-    const showcomment = true;
-    const commentBlock=(<div>
-        <h3>Comments ({comments.length})</h3> 
-         <ul>
-         {comments.map((comment,index)=>(
-             <li key={index}>{comment.text}</li>
-         ))}
-         </ul>
-     </div>)
-    return (<div className='container'>
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
-       {showcomment ? commentBlock:'NO'}
+    const deleteFeedback=(id)=>{
+        if(window.confirm("Are you sure you want to delete?"))
+        { setFeedback(feedback.filter((item)=>item.id !==id))
+        }
+    }
+    return (
+    <>
+    <Header />
+    <div className='container' >
+   
        
-    </div>
+    <FeedbackForm handleAdd={(addFeedback)}/>
+    <FeedbackStats feedback={feedback}/>
+    <FeedbackList feedback={feedback}
+     handleDelete={deleteFeedback}/>
+  
+ {/*
+    <AboutPages/>
+    
+    <AboutIconLink/> */}
+     </div>
+     </>
     )
 }
 
